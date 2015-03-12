8ad2b0ecfbe102a34b23c8c8a2d318b091a018e1
Matthew Gallo <mdgallo07@gmail.com>
2015-03-12 14:27:52 -0400

    Heroku fix api link

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,13 +7,13 @@ class EventsController < ApplicationController
     p params[:user_location]
     @user_location = params[:user_location]
     
-    location_api = HTTParty.get "http://api.songkick.com/api/3.0/search/locations.json?query=#{@user_location}&apikey=QG143a2Qf7zybpnb"
+    location_api = HTTParty.get URI.encode("http://api.songkick.com/api/3.0/search/locations.json?query=#{@user_location}&apikey=QG143a2Qf7zybpnb")
     puts "You're searching for #{@user_location}:".upcase
     # DISPLAYS JSON DATA LOCATION_ID!!!! WOOOOOO
     location_id = location_api["resultsPage"]["results"]["location"][0]["metroArea"]["id"]
     ap location_id
 
-    upcoming_events = HTTParty.get "http://api.songkick.com/api/3.0/metro_areas/#{location_id}/calendar.json?apikey=QG143a2Qf7zybpnb"
+    upcoming_events = HTTParty.get URI.encode("http://api.songkick.com/api/3.0/metro_areas/#{location_id}/calendar.json?apikey=QG143a2Qf7zybpnb")
     
     
     # Displays all upcoming events for that location:
