4ef985fd59e8553f2fbcdcfa0e76bd9619a938fa
Matthew Gallo <mdgallo07@gmail.com>
2015-04-01 22:17:32 -0400

    Moving logic from controller to model...

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,7 +13,47 @@ end
 
 
 
+# if artist_api['resultsPage']['totalEntries'] > 0
+#       @artist_id = artist_api["resultsPage"]["results"]["artist"][0]["id"]
+#       ap @artist_id
 
+#       artist_events = HTTParty.get URI.encode("http://api.songkick.com/api/3.0/artists/#{@artist_id}/calendar.json?apikey=QG143a2Qf7zybpnb")
+    
+#       @artist_event_details = artist_events["resultsPage"]["results"]["event"]
+    
+#       # ap @artist_event_details
+
+    
+#       # If the artist has upcoming events do all this stuff.
+#       if @artist_id && @artist_event_details != nil
+#         @artist_venue_lat = @artist_event_details[0]['venue']['lat']
+#         @artist_venue_lng = @artist_event_details[0]['venue']['lng']
+      
+#         # Create counter so I have an ID:
+#         counter = 0
+#         @artist_event_details.each do |event|
+#           counter += 1
+#           event.merge!({'counter_id' => counter})
+#           event_json = JSON.generate event
+#           event_id = counter
+#           @artist_performance_id = event['id']
+#           # ap event_json
+#           # ap event_id
+#           # If the event already exists in db,
+#           # don't create it, use what is already in the db
+#           # else 
+#           # search for it from the API.
+#           # end
+
+#             ap @artist_performance_id
+          
+#             if Artist.where(artist_performance_id: @artist_performance_id).length < 1
+#               Artist.create({ artist_performance_id: @artist_performance_id, event_artist: @artist_name, event_json: event })
+#             end 
+#         end
+#         @artists = Artist.where("event_artist LIKE ?", "%#{params[:artist_name]}%")
+#       end
+#     end
 
 
 
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -7,7 +7,42 @@ end
 
 
 
+# if venue_api["resultsPage"]["totalEntries"] > 0
+#       @venue_id = venue_api["resultsPage"]["results"]["venue"][0]["id"]
+#       ap @venue_id
 
+#       venue_events = HTTParty.get URI.encode("http://api.songkick.com/api/3.0/venues/#{@venue_id}/calendar.json?apikey=QG143a2Qf7zybpnb")
+#       @venue_event_details = venue_events["resultsPage"]["results"]["event"]
+
+#       # Only if the venue has upcoming events.
+#       if @venue_event_details != nil
+#         @venue_lat = @venue_event_details[0]['venue']['lat']
+#         @venue_lng = @venue_event_details[0]['venue']['lng']
+#         # Create counter so I have an ID:
+#         counter = 0
+#         @venue_event_details.each do |event|
+#           counter += 1
+#           event.merge!({'counter_id' => counter})
+#           event_json = JSON.generate event
+#           event_id = counter
+#           @venue_performance_id = event['id']
+          
+
+#           # If the event already exists in db,
+#           # don't create it (get it from API)
+#           # else 
+#           # search for it from the API.
+#           # end
+
+#           if Venue.where( venue_performance_id: @venue_performance_id ).length < 1
+#             Venue.create({ venue_performance_id: @venue_performance_id, event_venue: @venue_name, event_json: event })
+#           end
+#         end
+#         @venues = Venue.where("event_venue LIKE ?", "%#{params[:venue_name]}%")
+        
+#         # @venues = Venue.paginate(:page => params[:page], :per_page => 20)
+#       end
+#     end
 
 
 
